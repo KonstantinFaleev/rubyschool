79042329810c77b1e77c365159699ee368accace
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-04-29 23:47:54 +0300

    lesson4

---
+++ Projects/lesson4/app1.rb
@@ -0,0 +1,13 @@
+print "Enter X: "
+x = gets.chomp
+
+print "Enter Y: "
+y = gets.to_i
+
+print "Enter Z: "
+z = gets.to_f
+
+puts "х = " + x.to_s + " y = " + y.to_s + " z = " + z.to_s
+puts "х = " + x.to_s
+puts "y = " + y.to_s
+puts "z = " + z.to_s
