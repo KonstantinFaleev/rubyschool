4d651b9d228832aa5a459ab0d1af428aec26f46d
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-05-06 23:53:26 +0300

    arrdemo

--- Projects/lesson9/defdemo6.rb
+++ Projects/lesson9/defdemo6.rb
@@ -1,5 +1,23 @@
 def get_command
-  :left
+  x = rand(1..4)
+
+  if x == 1
+    cmd = :left
+  end
+
+  if x == 2
+    cmd = :right
+  end
+
+  if x == 3
+    cmd = :up
+  end
+
+  if x == 4
+    cmd = :down
+  end
+
+  cmd
 end
 
 command = get_command
@@ -8,4 +26,19 @@ puts "Получена команда: #{command}"
 
 if command == :left
   puts "Робот едет влево"
+end
+
+
+if command == :right
+  puts "Робот едет вправо"
+end
+
+
+if command == :up
+  puts "Робот едет вверх"
+end
+
+
+if command == :down
+  puts "Робот едет вниз"
 end
