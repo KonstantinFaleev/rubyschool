47b3931bb83f0ee5e64157e9eb9fe8d13c605c77
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-05-09 22:27:24 +0300

    refactoring rockpaperscissors

--- Projects/lesson10/app8.rb
+++ Projects/lesson10/app8.rb
@@ -18,16 +18,22 @@ computer_choice = arr[rand(0..2)]
 matrix = [
     [:rock, :paper, :second_win],
     [:rock, :scissors, :first_win],
-    [:rock, :rock, :drow]
+    [:rock, :rock, :draw],
+    [:paper, :paper, :second_win],
+    [:paper, :scissors, :first_win],
+    [:paper, :paper, :draw],
+    [:scissors, :rock, :second_win],
+    [:scissors, :paper, :first_win],
+    [:scissors, :scissors, :draw]
 ]
 
-matrix.each do [item]
+matrix.each do |item|
   if item[0] == user_choice && item[1] == computer_choice
     if item[2] == :first_win
       puts "User wins!"
     elsif item[2] == :second_win
       puts "Computer wins!"
-    elsif item[2] == :drow
+    elsif item[2] == :draw
       puts "Drow!"
     end
     exit
