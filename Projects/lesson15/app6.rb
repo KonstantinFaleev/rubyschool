558183511fca35b839e312a50d7791558898a06d
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-05-13 21:58:43 +0300

    initialize super class

--- Projects/lesson15/app6.rb
+++ Projects/lesson15/app6.rb
@@ -1,20 +1,32 @@
 class Animal
+  def initialize name
+    @name = name
+  end
   def run
-    puts "Runnitg..."
+    puts "#{@name} is running..."
   end
 
   def jump
-    puts "Jumping..."
+    puts "#{@name} is jumping..."
   end
 end
 
 class Cat < Animal
+
+  def initialize
+    super 'cat'
+  end
+
   def say_meow
     puts "Meeeeow..."
   end
 end
 
 class Dog < Animal
+
+  def initialize
+    super 'dog'
+  end
   def bark
     puts "Woof..."
   end
