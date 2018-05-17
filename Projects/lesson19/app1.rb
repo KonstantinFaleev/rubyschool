8d68d55b352a165eff033380c2a35b82ff2a3deb
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-05-17 21:24:50 +0300

    sinatra login password

--- Projects/lesson19/app1.rb
+++ Projects/lesson19/app1.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
 get '/' do
-  "Hi"
+  erb :index
+end
+
+post '/' do
+  @login = params[:aaa]
+  @password = params[:bbb]
+
+  if @login == 'admin' && @password == 'secret'
+    erb :welcome
+  else
+
+  erb :index
+  end
 end
