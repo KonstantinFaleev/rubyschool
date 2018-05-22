c31a889bc359e07584b1d10fc837b220ebc37169
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-05-22 21:05:51 +0300

    lesson24

---
+++ Projects/lesson24/sinatra-bootstrap-clean/app.rb
@@ -0,0 +1,36 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @barber = params[:barber]
+  @color = params[:color]
+
+  @title = 'Thank you!'
+  @message = "Dear #{@username}, we'll be waiting for you at #{@datetime} "
+
+  f = File.open './public/users.txt', 'a'
+  f.write "User: #{@username}, Phone: #{@phone}, Date and time: #{@datetime}, Name barber: #{@barber}, Color: #{@color}"
+  f.close
+  erb :message
+end
