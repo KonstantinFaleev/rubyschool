52b2f7163296e68b0a568e3c7571e9884eaf1e64
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-05-23 20:40:49 +0300

    lesson25 , sqlite3

--- Projects/lesson24/sinatra-bootstrap-clean/app.rb
+++ Projects/lesson24/sinatra-bootstrap-clean/app.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pony'
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
@@ -48,4 +49,26 @@ post '/visit' do
   f.write "User: #{@username}, Phone: #{@phone}, Date and time: #{@datetime}, Name barber: #{@barber}, Color: #{@color}"
   f.close
   erb :message
+end
+
+post '/contact' do
+  Pony.mail(
+      :name => params[:name],
+      :mail => params[:mail],
+      :body => params[:body],
+      :to => 'a_lumbee@gmail.com',
+      :subject => params[:name] + " has contacted you",
+      :body => params[:message],
+      :port => '587',
+      :via => :smtp,
+      :via_options => {
+          :address              => 'smtp.gmail.com',
+          :port                 => '587',
+          :enable_starttls_auto => true,
+          :user_name            => 'lumbee',
+          :password             => 'p@55w0rd',
+          :authentication       => :plain,
+          :domain               => 'localhost.localdomain'
+      })
+  redirect '/visit'
 end
