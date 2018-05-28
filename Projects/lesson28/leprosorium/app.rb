3436938c9297e71dcd0aa9681e0f3ed889f017ce
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-05-29 01:57:26 +0300

    Add /details/...handler

--- Projects/lesson28/leprosorium/app.rb
+++ Projects/lesson28/leprosorium/app.rb
@@ -47,5 +47,10 @@ post '/new' do
   # Сохранение данных в БД
   @db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
   redirect to '/'
-  erb "You typed: #{content}"
+end
+
+get '/details/:post_id' do
+  post_id = params[:post_id]
+
+  erb "Displaying information for post with id #{post_id}"
 end
