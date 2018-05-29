a7088d3728329ce086bccefefb4fe1064800aeb7
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-05-29 23:35:11 +0300

    refactoring code

--- Projects/lesson28/leprosorium/app.rb
+++ Projects/lesson28/leprosorium/app.rb
@@ -1,4 +1,3 @@
-
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
@@ -9,78 +8,123 @@ def init_db
   @db.results_as_hash = true
 end
 
+# before вызывается каждый раз при перезагрузке
+# любой страницы
+
 before do
-  # Инициализация БД
+  # индициализация БД
   init_db
 end
 
-# Вызывается каждый раз при именении приложения:
-# когда изменился код страницы и перезагрузилась страница
-configure do
-  # Инициализация БД
-  init_db
-  # создает таблицу если таблица существует
-  @db.execute 'CREATE TABLE IF NOT EXISTS
-       "Posts"
-       ("id" INTEGER PRIMARY KEY AUTOINCREMENT,
-        "created_date" DATE,
-        "content" TEXT
-       );'
+# configure вызывается каждый раз при конфигурации приложения:
+# когда изменился код программы И перезагрузилась страница
 
-  # создает таблицу если таблица существует
-  @db.execute 'CREATE TABLE IF NOT EXISTS
-       "Comments"
-       ("id" INTEGER PRIMARY KEY AUTOINCREMENT,
-        "created_date" DATE,
-        "content" TEXT,
-        "post_id" INTEGER
-       );'
+configure do
+  # инициализация БД
+  init_db
+
+  # создает таблицу если таблица не существует
+  @db.execute 'create table if not exists Posts
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		created_date DATE,
+		content TEXT
+	)'
+
+  # создает таблицу если таблица не существует
+  @db.execute 'create table if not exists Comments
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		created_date DATE,
+		content TEXT,
+		post_id integer
+	)'
 end
 
 get '/' do
+  # выбираем список постов из БД
+
   @results = @db.execute 'select * from Posts order by id desc'
-	erb :index
+
+  erb :index
 end
+
+# обработчик get-запроса /new
+# (браузер получает страницу с сервера)
 
 get '/new' do
   erb :new
 end
 
+# обработчик post-запроса /new
+# (браузер отправляет данные на сервер)
+
 post '/new' do
-  # Получаем переменную из пост запроса
+  # получаем переменную из post-запроса
   content = params[:content]
+
   if content.length <= 0
-    @error = "Type post text"
+    @error = 'Type post text'
     return erb :new
   end
-  # Сохранение данных в БД
+
+  # сохранение данных в БД
+
   @db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
+
+  # перенаправление на главную страницу
+
   redirect to '/'
 end
 
+# вывод информации о посте
+
 get '/details/:post_id' do
+
   # получаем переменную из url'a
   post_id = params[:post_id]
+
   # получаем список постов
   # (у нас будет только один пост)
   results = @db.execute 'select * from Posts where id = ?', [post_id]
+
   # выбираем этот один пост в переменную @row
   @row = results[0]
 
+  # выбираем комментарии для нашего поста
   @comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
+
   # возвращаем представление details.erb
   erb :details
 end
-# обработчик пост запроса /details/...
-# браузер отправляет данные на сервер , мы их принимаем
+
+# обработчик post-запроса /details/...
+# (браузер отправляет данные на сервер, мы их принимаем) 
+
 post '/details/:post_id' do
+
   # получаем переменную из url'a
   post_id = params[:post_id]
-  # Получаем переменную из пост запроса
+
+  # получаем переменную из post-запроса
   content = params[:content]
 
-  @db.execute 'insert into Comments (content, created_date, post_id) values (?, datetime(),?)', [content, post_id]
-  erb "You type comment #{content} for post #{post}"
+  # сохранение данных в БД
+
+  @db.execute 'insert into Comments
+		(
+			content,
+			created_date,
+			post_id
+		)
+			values
+		(
+			?,
+			datetime(),
+			?
+		)', [content, post_id]
+
+  # перенаправление на страницу поста
 
   redirect to('/details/' + post_id)
 end
